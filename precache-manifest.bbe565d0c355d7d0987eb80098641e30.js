95c9c8e31da3370b02f979c417093c433390fc9a
Shuxrat Begmatov <hp@mm50.uzbektourism.local>
2023-12-04 14:28:25 +0500

    fix

---
+++ precache-manifest.bbe565d0c355d7d0987eb80098641e30.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88b03e962aaf2b7254e5",
+    "url": "/css/about.ccd6b9d3.css"
+  },
+  {
+    "revision": "1f5bfbee803e7d6e8e84",
+    "url": "/css/app.c93fc8cb.css"
+  },
+  {
+    "revision": "135cf4b35edcd987a942",
+    "url": "/css/chunk-vendors.db0db735.css"
+  },
+  {
+    "revision": "e5f18cb987385760e628a9671f975412",
+    "url": "/fonts/Inter-Medium.e5f18cb9.ttf"
+  },
+  {
+    "revision": "188c253e810c9aecfeaa12795d667868",
+    "url": "/img/Logomark.188c253e.svg"
+  },
+  {
+    "revision": "fafae29851b13f230c18d9b9f67ca81c",
+    "url": "/img/dash_bg.fafae298.png"
+  },
+  {
+    "revision": "b8fc6da25f665eac6416d785e11b15b9",
+    "url": "/img/image1.b8fc6da2.png"
+  },
+  {
+    "revision": "71a76c4237444b0f3b540f0a2c46179c",
+    "url": "/img/login_bg.71a76c42.png"
+  },
+  {
+    "revision": "bfbd518120380f467eb0fe508619d8f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88b03e962aaf2b7254e5",
+    "url": "/js/about.5c5ef79e.js"
+  },
+  {
+    "revision": "1f5bfbee803e7d6e8e84",
+    "url": "/js/app.0c386de2.js"
+  },
+  {
+    "revision": "135cf4b35edcd987a942",
+    "url": "/js/chunk-vendors.f6f971a5.js"
+  },
+  {
+    "revision": "fba0e2185fa014e4265947d9e05d5a01",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
